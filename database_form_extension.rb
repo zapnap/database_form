a180488845c04bad48f567eb4e3434298dc42b98
Nick Plante <nap@zerosum.org>
2009-01-09 14:08:28 -0500

    update version and repo location

--- database_form_extension.rb
+++ database_form_extension.rb
@@ -1,7 +1,7 @@
 class DatabaseFormExtension < Radiant::Extension
-  version "0.2"
+  version "0.2.1"
   description "Provides a page type for email contact and request forms and saves requests to a database (modified Mailer)."
-  url "http://ubikorp.com/projects"
+  url "http://github.com/zapnap/database_form"
 
   define_routes do |map|
     map.connect 'admin/form_responses/:action/:id', :controller => 'admin/form_responses'
